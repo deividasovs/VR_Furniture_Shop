11e686ba7559e0bbf880216b762acf54575ab2d2
Deividas Ovsianikovas <deividas.ovsianikovas.2020@mumail.ie>
2023-04-28 16:57:29 +0100

    Fixes door rotation

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -1,15 +1,18 @@
 extends StaticBody
 
 onready var anim_player = $AnimationPlayer
+var door_open = false
 
 # Anim not playing by default
 func _ready():
+	door_open = false
 	anim_player.stop()
 
-# Turns the light on
 func open_door():
-	print("Opening door")
-	anim_player.play("DoorRotation")
+	# Only animate if the animation hasn't played yet
+	if !door_open:
+		anim_player.play("DoorRotation")
+		door_open = true
 
 func _on_Area_body_entered(body):
 	open_door()
