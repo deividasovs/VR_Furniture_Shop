c45c80d775c172baf911b63d5229d9c98d35496c
Deividas Ovsianikovas <deividas.ovsianikovas.2020@mumail.ie>
2023-04-29 14:37:20 +0100

    UI popup

--- Scripts/platform.gd
+++ Scripts/platform.gd
@@ -2,17 +2,13 @@ extends StaticBody
 
 # references the light that is used for the lamp
 # IMPORTANT: If you rename your light, you will need to change this line.
-onready var test_platform = $TestPlatform
+onready var test_platform = $SampleUIPoppup
 
 # Turned off by default
 func _ready():
-	test_platform.transform.origin = Vector3(4, 0.8, 8)
+	test_platform.transform.origin = Vector3(8, -1, 5)
 	
 
 func _on_Area_body_entered(body):
-	print("abc")
-	test_platform.transform.origin = Vector3(8, 0.8, 8)
-
-
-func _on_Area_body_exited(body):
-	test_platform.transform.origin = Vector3(4, 0.8, 8)
+	# Could probably animate this up instead
+	test_platform.transform.origin = Vector3(8, 1, 5)
